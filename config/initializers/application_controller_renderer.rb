98c1254704d8dcfd256a46659d95b14198e10b99
Miz-Yam <gyakusyuboo@gmail.com>
2021-02-24 14:59:41 +0000

    Made list page and detail page

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'bc41948facd5808cddd12fe2348b72a131678390f2f791aba16766c4ce6c7e8f2162f7b59f23e2680245066c8b86a636048884c4982247bca566b4d005b888cd'
